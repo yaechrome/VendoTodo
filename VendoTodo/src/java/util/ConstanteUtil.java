997b70897250e3566aeb06758443a4d9693148d6
Joselyn Moya <joselyn.moyap@gmail.com>
2017-11-19 21:19:51 -0300

    Se agrega clase de constantes

---
+++ VendoTodo/src/java/util/ConstanteUtil.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author nippo
+ */
+public class ConstanteUtil {
+    public static final String CONSULTA_URL_FILE = "/paginas/consulta.jsp";
+    public static final String CONSULTA_URL_PAGE = "http://localhost:8080/vendo-todo/privado/Consultas";
+    public static final String HOME_URL_PAGE = "http://localhost:8080/vendo-todo/privado/Home";
+}
