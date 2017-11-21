b0f73b3d47eb2ffe6507da8737781a68dbf013ee
Joselyn Moya <joselyn.moyap@gmail.com>
2017-11-20 23:09:25 -0300

    se arregla logout

--- VendoTodo/src/java/filter/SessionFilter.java
+++ VendoTodo/src/java/filter/SessionFilter.java
@@ -18,6 +18,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import servlet.Login;
 import util.ConstanteUtil;
@@ -29,21 +30,30 @@ import static util.ConstanteUtil.*;
  */
 @WebFilter(filterName = "SessionFilter", urlPatterns = {"/privado/*"})
 public class SessionFilter implements Filter {
-
+    
     @Override
     public void doFilter(ServletRequest request,
             ServletResponse response,
             FilterChain chain) throws IOException, ServletException {
-
+        
         HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        httpResponse.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+        httpResponse.setDateHeader("Expires", 0); // Proxies.
         HttpSession session = httpRequest.getSession();
         UsuarioDto usuarioAutenticado = (UsuarioDto) session.getAttribute(ConstanteUtil.LOGIN_USUARIO);
+        String url = ((HttpServletRequest) request).getRequestURL().toString();
         if (usuarioAutenticado == null) {
             System.err.println("La sesión no registra ningún usuario autenticado");
             System.err.println("Se redirecciona usuario a página de login");
-            request.getRequestDispatcher(ConstanteUtil.LOGIN_URL_FILE).forward(request, response);
+            if (LOGIN_URL_PAGE.equals(url)) {
+                request.getRequestDispatcher(ConstanteUtil.LOGIN_URL_FILE).forward(request, response);
+            } else {
+                httpResponse.sendRedirect(LOGIN_URL_PAGE);
+            }
         } else {
-            if (!resolvePerfil(usuarioAutenticado.getCodigoPerfil(), ((HttpServletRequest) request).getRequestURL().toString())) {
+            if (!resolvePerfil(usuarioAutenticado.getCodigoPerfil(), url)) {
                 System.err.println("La sesión iniciada no tiene permisos para acceder a la página solicitada");
                 request.getRequestDispatcher(ConstanteUtil.HOME_URL_SERVLET).forward(request, response);
             } else {
@@ -51,17 +61,17 @@ public class SessionFilter implements Filter {
             }
         }
     }
-
+    
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         // ninguna configuración que realizar al inicio
     }
-
+    
     @Override
     public void destroy() {
         // ninguna acción que realizar al finalizar
     }
-
+    
     private boolean resolvePerfil(int idPerfil, String url) {
         switch (idPerfil) {
             case 1:
@@ -70,13 +80,13 @@ public class SessionFilter implements Filter {
                 return validaPerfilConsulta(url);
             case 3:
                 return validaPerfilVenta(url);
-
+            
             default:
                 break;
         }
         return false;
     }
-
+    
     public boolean validaPerfilConsulta(String url) {
         switch (url) {
             case CONSULTA_URL_PAGE:
@@ -87,7 +97,7 @@ public class SessionFilter implements Filter {
                 return false;
         }
     }
-
+    
     public boolean validaPerfilVenta(String url) {
         switch (url) {
             case VENTA_URL_PAGE:
